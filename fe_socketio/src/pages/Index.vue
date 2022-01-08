4531b0bdd6d9eaef819a32723327b15e047a6250
Selman <selmanbaskaya1@gmail.com>
2022-01-08 14:00:54 +0300

    index page img resized

--- fe_socketio/src/pages/Index.vue
+++ fe_socketio/src/pages/Index.vue
@@ -3,7 +3,7 @@
     <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+      style="width: 400px; height: 400px"
     />
   </q-page>
 </template>
